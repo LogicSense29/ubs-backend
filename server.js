db53c98b0a44bffa5abc364e96156253f92eca6c
MAC <mac@MACs-MacBook.local>
2024-03-05 18:36:09 +0100

    added cors

--- server.js
+++ server.js
@@ -13,7 +13,12 @@ app.use(cors());
 app.use(express.json());
 db.connect();
 
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://ubs-personality-test.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
 
 
 app.use("/api/users", authRoute)
