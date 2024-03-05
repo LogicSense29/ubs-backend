b9e047e6be4964a725a131c604d41c67b00a6ad9
MAC <mac@MACs-MacBook.local>
2024-03-05 22:19:28 +0100

    header acao

--- server.js
+++ server.js
@@ -17,6 +17,13 @@ db.connect();
 app.use(cors(corsOptions));
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://ubs-personality-test.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+
 
 
 
