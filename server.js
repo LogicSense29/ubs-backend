c16d4fe2112c7a7460f1997960f012bfeb6efc04
MAC <mac@MACs-MacBook.local>
2024-03-05 22:35:48 +0100

    again

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = `${process.env.PORT}`
 
 const corsOptions = {
-    origin: "https://ubs-personality-test.vercel.app",
+    origin: "https://ubs-personality-test.vercel.app/",
   };
 
 
@@ -17,7 +17,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://ubs-personality-test.vercel.app');
+    res.header('Access-Control-Allow-Origin', 'https://ubs-personality-test.vercel.app/');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
