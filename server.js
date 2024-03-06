d868da4ccdd0ff67ac060242839d6fe3e1d4bfd1
MAC <mac@MACs-MacBook.local>
2024-03-06 08:46:39 +0100

    proxyapi

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors =require("cors")
 const db = require("./db");
 const authRoute = require("./routes/auth")
 const resultRoute = require("./routes/results")
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 const PORT = `${process.env.PORT}`
@@ -13,6 +14,18 @@ const corsOptions = {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     // credentials: true,
   };
+
+// Create a proxy middleware that forwards requests to the API server
+const apiProxy = createProxyMiddleware('/api', {
+    target: 'https://ubs-pt.cyclic.app',
+    changeOrigin: true,
+    pathRewrite: {
+        '^/api': '/api', // Optional: Rewrite the path if needed
+    },
+  });
+  
+  app.use(apiProxy);
+  
 
 
 app.use(cors(corsOptions));
