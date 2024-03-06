83b74427aa012fc74c51c3405f610a72930f011e
MAC <mac@MACs-MacBook.local>
2024-03-06 09:12:51 +0100

    adding a get route

--- server.js
+++ server.js
@@ -22,6 +22,10 @@ const PORT = `${process.env.PORT}`
 app.options('*', cors()) 
 app.use(express.json());
 
+app.get("/", (req,res) =>{
+    res.json({message: "This is an App for lovers"})
+})
+
   
 db.connect();
 
