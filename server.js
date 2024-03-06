ae6af8612a2573a3934601f4db89c1c6525c9476
MAC <mac@MACs-MacBook.local>
2024-03-06 09:00:23 +0100

    adding option

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const cors =require("cors")
 const db = require("./db");
 const authRoute = require("./routes/auth")
 const resultRoute = require("./routes/results")
-const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 const PORT = `${process.env.PORT}`
@@ -15,20 +14,12 @@ const corsOptions = {
     // credentials: true,
   };
 
-// Create a proxy middleware that forwards requests to the API server
-const apiProxy = createProxyMiddleware('/api', {
-    target: 'https://ubs-pt.cyclic.app',
-    changeOrigin: true,
-    pathRewrite: {
-        '^/api': '/api', // Optional: Rewrite the path if needed
-    },
-  });
-  
-  app.use(apiProxy);
+
   
 
 
 app.use(cors(corsOptions));
+app.options('*', cors()) 
 app.use(express.json());
 
   
