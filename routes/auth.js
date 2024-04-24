c0c54e8cc4eba575f4500dd4a7f68ce42594f447
MAC <mac@MACs-MacBook.local>
2024-04-24 16:01:25 +0100

    adding setHeader

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const router = express.Router();
-const {addUserValidation} = require("../validators/auth")
-const vMW = require("../middleware/authMW")
-const {getUsers, addUsers} = require("../controllers/auth")
-
+const { addUserValidation } = require("../validators/auth");
+const vMW = require("../middleware/authMW");
+const { getUsers, addUsers } = require("../controllers/auth");
 
 router.post("/login", getUsers);
 
 router.post("/register", addUserValidation, vMW, addUsers);
 
+router.get("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+  );
+});
+
 module.exports = router;
-
-
 
 // router.delete("/", deleteUser);
